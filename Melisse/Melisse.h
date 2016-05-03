f2e6b1b892365f145ee6126719e876f67e7eda6d
Raphaël C <ddaeke-bitbucket@yahoo.fr>
2016-05-03 09:35:35 +0200

    Changement du nom du projet en 'Mélisse'

---
+++ Melisse/Melisse.h
@@ -0,0 +1,18 @@
+//
+//  Melisse.h
+//  Melisse
+//
+//  Created by Raphaël Calabro on 02/05/2016.
+//  Copyright © 2016 Raphaël Calabro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Melisse.
+FOUNDATION_EXPORT double MelisseVersionNumber;
+
+//! Project version string for Melisse.
+FOUNDATION_EXPORT const unsigned char MelisseVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Melisse/PublicHeader.h>
+
