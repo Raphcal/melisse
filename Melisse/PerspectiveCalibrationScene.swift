14ae57b8c401fd666671a28c7d66f3598c2dcf3b
Raphaël C <ddaeke-bitbucket@yahoo.fr>
2016-05-27 15:26:54 +0200

    Début d'une scène de calibration.

---
+++ Melisse/PerspectiveCalibrationScene.swift
@@ -0,0 +1,23 @@
+//
+//  PerspectiveCalibrationScene.swift
+//  Melisse
+//
+//  Created by Raphaël Calabro on 27/05/2016.
+//  Copyright © 2016 Raphaël Calabro. All rights reserved.
+//
+
+import GLKit
+
+public class PerspectiveCalibrationScene : Scene {
+    
+    public let backgroundColor = Color<GLfloat>(white: 1)
+    
+    public func updateWith(timeSinceLastUpdate: NSTimeInterval) {
+        // TODO: Mise à jour.
+    }
+    
+    public func draw() {
+        // TODO: Affichage.
+    }
+    
+}
